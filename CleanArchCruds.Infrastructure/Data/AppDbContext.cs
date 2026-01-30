9dd418eb774b6946b30d9c17e0953dd4d0907260
Myourkiu <pedrofigsesteves@gmail.com>
2026-01-30 19:17:39 -0300

    feat: initial commit

--- CleanArchCruds.Infrastructure/Data/AppDbContext.cs
+++ CleanArchCruds.Infrastructure/Data/AppDbContext.cs
@@ -11,6 +11,5 @@ public class AppDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-        // Configurações das entidades
     }
 }
